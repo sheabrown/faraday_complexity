68806c0375c15cd932e0eab75d259d8772129678
Jacob Isbell <jacob.isbell@uiowa.edu>
2017-04-04 10:59:31 -0500

    Modified to correctly use to_categorical

--- python/v2_inception1d.py
+++ python/v2_inception1d.py
@@ -14,28 +14,36 @@ from keras import backend as K
 from keras.optimizers import SGD
 from sklearn.utils import shuffle
 from keras import optimizers
+from keras.utils import plot_model
+import pydot
+import graphviz
 
 # input spectrum dimensions
-spec_length = 201
+spec_length = 200
 input_shape = (spec_length, 2)
 batch_size = 10
 nb_classes = 2
 nb_epoch = 5
 
 
+#plot the model
+
 def read_data():
 	# Load some test data
-	X_train=np.load('../data/train/X_data.npy')
+	X_train=np.load('../Sim_data/x_7_Normalized_7.npy')
 	'''q = np.load('../data/train/X_data.npy')
 	u = np.load('../data/train/X_data.npy')
 	X_train = np.array([q,u])'''
-	y_train=np.load('../data/train/label.npy')
+	y_train=np.load('../Sim_data/y_7_7.npy')
 
-	X_test=np.load('../data/test/X_data.npy')
+	X_test=np.load('../Sim_data/x_7_Normalized_5.npy')
 	'''q = np.load('../data/test/X_data.npy')
 	u = np.load('../data/test/X_data.npy')
 	X_test = np.array([q,u])'''
-	y_test=np.load('../data/test/label.npy')
+	y_test=np.load('../Sim_data/y_7_5.npy')
+	
+	Y_train = np_utils.to_categorical(y_train, nb_classes)
+	Y_test = np_utils.to_categorical(y_test, nb_classes)
 
 	#shuffle the data randomly
 	X_train, y_train = shuffle(X_train, y_train, random_state=0)
@@ -53,27 +61,48 @@ def incept():
 	nb_filter = 32
 	conv1x1 = Convolution1D(nb_filter=nb_filter, filter_length=1, subsample_length=1)(input)
 
-	inception_1a1x1 = Convolution1D(nb_filter=nb_filter, filter_length=1, subsample_length=1)(conv1x1)
+	inception_1a1x1 = Convolution1D(nb_filter=nb_filter, filter_length=1, subsample_length=1, activation='relu')(conv1x1)
 
 	#23x23 convolution layer
 	inception_1a23x23_reduce = Convolution1D(nb_filter=32, filter_length=1)(conv1x1)
-	inception_1a23x23 = Convolution1D(nb_filter=nb_filter, filter_length=23, subsample_length=1,border_mode='same')(inception_1a23x23_reduce)
+	inception_1a23x23 = Convolution1D(nb_filter=nb_filter, filter_length=3, subsample_length=1,border_mode='same', activation='relu')(inception_1a23x23_reduce)
 
 	#46x46 convolution layer
 	inception_1a46x46_reduce = Convolution1D(nb_filter=32, filter_length=1)(conv1x1)
-	inception_1a46x46 = Convolution1D(nb_filter=nb_filter, filter_length=46, subsample_length=1,border_mode='same')(inception_1a46x46_reduce)
+	inception_1a46x46 = Convolution1D(nb_filter=nb_filter, filter_length=5, subsample_length=1,border_mode='same', activation='relu')(inception_1a46x46_reduce)
 
 	#pooling layer
 	inception_1a_pool = MaxPooling1D(pool_length=3, stride=1, border_mode='same')(conv1x1)
-	inception_1a_pool1x1 = Convolution1D(nb_filter=nb_filter, filter_length=1, border_mode='same')(inception_1a_pool)
+	inception_1a_pool1x1 = Convolution1D(nb_filter=nb_filter, filter_length=1, border_mode='same', activation='relu')(inception_1a_pool)
 
 	merge1 = klayers.concatenate([inception_1a1x1, inception_1a23x23, inception_1a46x46, inception_1a_pool1x1])
 
-	d = Dense(256, activation='relu',name='1')(merge1)
-	d = Dense(256, activation='relu',name='2')(d)
+	'''#---------- layer two ----------------
+	conv1x1 = Convolution1D(nb_filter=nb_filter, filter_length=1, subsample_length=1)(merge1)
+
+	inception_2a1x1 = Convolution1D(nb_filter=nb_filter, filter_length=1, subsample_length=1)(conv1x1)
+
+	#23x23 convolution layer
+	inception_2a23x23_reduce = Convolution1D(nb_filter=32, filter_length=1)(conv1x1)
+	inception_2a23x23 = Convolution1D(nb_filter=nb_filter, filter_length=23, subsample_length=1,border_mode='same')(inception_2a23x23_reduce)
+
+	#46x46 convolution layer
+	inception_2a46x46_reduce = Convolution1D(nb_filter=32, filter_length=1)(conv1x1)
+	inception_2a46x46 = Convolution1D(nb_filter=nb_filter, filter_length=64, subsample_length=1,border_mode='same')(inception_2a46x46_reduce)
+
+	#pooling layer
+	inception_2a_pool = MaxPooling1D(pool_length=3, stride=1, border_mode='same')(conv1x1)
+	inception_2a_pool1x1 = Convolution1D(nb_filter=nb_filter, filter_length=1, border_mode='same')(inception_2a_pool)
+
+	merge2 = klayers.concatenate([inception_2a1x1, inception_2a23x23, inception_2a46x46, inception_2a_pool1x1])'''
+
+
+	conv1x1 = Convolution1D(nb_filter=nb_filter, filter_length=1, subsample_length=1)(merge1)
+	d = Dense(256, activation='tanh',name='1')(conv1x1)
+	d = Dense(256, activation='tanh',name='2')(d)
 	d = Dropout(0.5)(d)
 	d = Flatten()(d)
-	d = Dense(128, activation='relu',name='3')(d)
+	d = Dense(128, activation='tanh',name='3')(d)
 
 	out = Dense(1, activation='softmax', name='main_out')(d)
 
@@ -88,10 +117,11 @@ if __name__ == '__main__':
 	model = incept()
 	model.compile(optimizer='adadelta',
               loss='binary_crossentropy',
-              metrics=['accuracy'])
-	model.fit(X_train, Y_train, batch_size=batch_size, nb_epoch=nb_epoch,
-          verbose=1)#, validation_data=(X_test, Y_test))
-	score = model.evaluate(X_test, Y_test, verbose=0)
+              metrics=['binary_accuracy'])
+	model.fit(X_train, Y_train, batch_size=batch_size, nb_epoch=nb_epoch,verbose=1)#, validation_data=(X_test, Y_test))
+	#score = model.evaluate(X_test, Y_test, verbose=0)
+	plot_model(model, to_file='model.png')
+
 
 	# The predict_classes function outputs the highest probability class
 	# according to the trained classifier for each input example.
