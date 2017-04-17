bae1268164e296e61a4e651396843b2aba042e69
Bergerud <bergerud@r-lnx701.physics.uiowa.edu>
2017-04-17 12:22:27 -0500

    removed some old files

--- regularized/validData.py
+++
@@ -1,5 +0,0 @@
-from possum import *
-
-spec = possum()
-spec._generateParams(N=10000, pcomplex=0.50, fluxMin=0.8, noiseMax=0.1, seed=5555)
-spec._simulateNspec(save=True, dir='data/valid/V1/', timeit=True)
