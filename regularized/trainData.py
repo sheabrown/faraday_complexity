eaccd118c3b98f311f91db6387de67ef5911dea0
Bergerud <bergerud@r-lnx701.physics.uiowa.edu>
2017-04-17 12:22:00 -0500

    removed some old files

--- regularized/trainData.py
+++
@@ -1,5 +0,0 @@
-from possum import *
-
-spec = possum()
-spec._generateParams(N=30000, pcomplex=0.50, fluxMin=0.8, noiseMax=0.1, seed=2222)
-spec._simulateNspec(save=True, dir='data/train/V1/', timeit=True)
