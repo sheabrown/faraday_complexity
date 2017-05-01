5a7e52f4b8918c585f02a9581e51938e199bf7c2
Jacob Isbell <jacob.isbell@uiowa.edu>
2017-05-01 12:16:59 -0500

    Finally added the functions from cutoff_plots.py to work in the class-based format.

--- final/plots.py
+++ final/plots.py
@@ -4,6 +4,8 @@ import seaborn as sns
 import pandas as pd
 from keras.utils import plot_model
 from sklearn.metrics import confusion_matrix, f1_score, roc_curve
+from keras.models import load_model
+
 
 class plots:
 	# ===================================================
@@ -163,95 +165,294 @@ class plots:
 		self.dfLog_ = pd.read_csv(logfile, index_col=0)
 
 
-	def _plotLoss(self, logfile=None, save=False, imfile='loss_vs_epoch.pdf', losstype='adadelta'):
-		"""
-		Function for plotting the training and validation loss
-		as a function of the epoch.
-
-		To call:
-			_plotLoss(logfile, ...)
-
-		Parameters:
-			logfile		name of the logfile
-			save		(boolean) save image
-			imfile		name/path of file
-
-			losstype	??????????????????
-
-		"""
-
-		# ===================================================
-		#	Read in the Log File
-		# ===================================================
-		try:
-			self.dfLog_
-		except:
-			self._loadLog(logfile)
-
-		# ===================================================
-		# 	Plot the losses as a function of epoch
-		# ===================================================
+	def _plotLoss(self, logfile, save=False, outname='loss_vs_epoch.pdf', losstype = 'adadelta'):
+		#----- Read in the Log File ----------
+		self._loadLog(logfile)
+		# -------------- Initialize the Graph ---------
 		fig = plt.figure()
-		plt.rc('text', usetex=True)
-		plt.rc('font', family='serif')
-		plt.xlabel(r'Epoch')
-		plt.ylabel(r'Loss (%s)'%(losstype))
-		plt.plot(self.dfLog_.index, self.dfLog_['loss'], label='Training Loss')
-		plt.plot(self.dfLog_.index, self.dfLog_['val_loss'], label='Validation Loss')
+		plt.xlabel('Epoch')
+		plt.ylabel('Loss (%s)'%(losstype))
+		plt.plot(range(len(self.dfLog_['loss'])), self.dfLog_['loss'], label='Training Loss')
+		plt.plot(range(len(self.dfLog_['val_loss'])), self.dfLog_['val_loss'], label='Validation Loss')
 		plt.legend(fontsize=7.5)
-
 		if save:
-			plt.savefig(imfile)
+			plt.savefig(outname)
+			plt.close()
+		else:
+			plt.show()
+			plt.close()
+	def _plotAcc(self, logfile, save=False, outname='acc_vs_epoch.pdf'):
+		#----- Read in the Log File ----------
+		self._loadLog(logfile)
+		# -------------- Initialize the Graph ---------
+		fig = plt.figure()
+		plt.xlabel('Epoch')
+		plt.ylabel('Binary Accuracy ')
+		plt.plot(range(len(self.dfLog_['binary_accuracy'])), self.dfLog_['binary_accuracy'], label='Training Binary Accuracy')
+		plt.plot(range(len(self.dfLog_['val_binary_accuracy'])), self.dfLog_['val_binary_accuracy'], label='Validation Binary Accuracy')
+		plt.legend(fontsize=7.5)
+		if save:
+			plt.savefig(outname)
 			plt.close()
 		else:
 			plt.show()
 			plt.close()
 
-
-	def _plotAcc(self, logfile=None, save=False, imfile='acc_vs_epoch.pdf'):
+	def _loadModel(self, modelFile):
 		"""
-		Function for plotting the training and validation accuracy
-		as a function of the epoch.
+		Function for loading a keras model from a file.
 
 		To call:
-			_plotLoss(logfile, ...)
+			_loadModel(modelFile)
 
 		Parameters:
-			logfile		name of the logfile
-			save		(boolean) save image
-			imfile		name/path of file
+			modelFile
 		"""
-		# ===================================================
-		#	Read in the Log File
-		# ===================================================
-		try:
-			self.dfLog_
-		except:
-			self._loadLog(logfile)
+		self.model = load_model(modelFile)
 
-		# ===================================================
-		# 	Plot the accuracy as a function of epoch
-		# ===================================================
-		fig = plt.figure()
-		plt.rc('text', usetex=True)
-		plt.rc('font', family='serif')
-		plt.xlabel(r'$\rm Epoch$')
-		plt.ylabel(r'$\rm Binary Accuracy$')
-		plt.plot(self.dfLog_.index, self.dfLog_['binary_accuracy'], label='Training Binary Accuracy')
-		plt.plot(self.dfLog_.index, self.dfLog_['val_binary_accuracy'], label='Validation Binary Accuracy')
-		plt.legend(fontsize=7.5)
+	def _loadData(self, directory):
+		"""
+		Function for loading data arrays from a directory.
+
+		To call:
+			_loadModel(directory)
+
+		Parameters:
+			directory
+		"""
+		self.X_data = np.load(directory+'X_data.npy')
+		self.Y_data = np.load(directory+'label.npy')
+
+		#------ creation params --------
+		self.chi_data = np.load(directory+'chi.npy')
+		self.depth_data = np.load(directory+'depth.npy')
+		self.flux_data = np.load(directory+'flux.npy')
+		self.q_data = np.load(directory+'Q_data.npy')
+		self.s_data = np.load(directory+'S_data.npy')
+		self.sig_data = np.load(directory+'sig.npy')
+		self.u_data = np.load(directory+'U_data.npy')
+
+	def _format_param_name(self,param_name):
+		"""
+		Function for formatting a string parameter name (chi, depth, etc....) to LateX
+		form for plot labels.
+
+		To call:
+			_format_param_name(param_name)
+
+		Parameters:
+			param_name
+		"""
+		if param_name == 'sigma':
+			return r'$\sigma$'
+		elif param_name == 'chi':
+			return r'$\Delta\chi$'
+		elif param_name == 'flux':
+			return r'$\Delta F$'
+		elif param_name == 'depth':
+			return  r'$\Delta \phi$'
+		else:
+			return param_name
+
+	def _make_cut(self, param_array, param_name,num_cut=10,prob=0.5, save=False):
+		"""
+		Function for cutting along a single parameter value to test the model's performance over
+		a parameter range. For aid in finding parameter space that model works with certainty within.
+		Makes a plot showing the True Positive (TP) and True Negative (TN) rates as a function of the
+		supplied parameter.
+
+		To call:
+			_make_cut(param_array, param_name,num_cut, prob, save)
+
+		Parameters:
+			param_array
+			param_name
+			OPTIONAL:
+				num_cut -- number of cuts to make along the parameter
+				prob -- probability cutoff to classify as complex or simple
+				save -- True if want to save a .pdf
+		"""
+		cut_array = param_array
+		# ----------- sigma and other params are formatted differently, this handles either case ------
+		try:
+			cut_vals = np.linspace(0.,np.max(cut_array)[0]*.9,num_cut)
+			oned =False
+		except:
+			cut_vals = np.linspace(0.,np.max(cut_array)*.9,num_cut)
+			oned = True
+		matrix_vals = []
+		# --------- make a series of cuts and save results for plotting ----------
+		for c in cut_vals:
+			print (c)
+			#do the cut
+			float_check = type(0.1); tuple_check = type((0,1))
+			postcut = [];kept=[]
+			for i in range(len(cut_array)):
+				val = cut_array[i]
+				# ---------- once again handle tuples or floats depending on parameter format ----------
+				if type(val) == tuple_check:
+					if abs(val[0]-val[1]) >= c:
+						postcut.append(abs(val[0]-val[1]))
+						kept.append(i)
+				else:
+					if val >= c:
+						postcut.append(val)
+						kept.append(i)
+			try:
+				# -------- the subset of data --------------
+				X_new=np.array([self.X_data[k] for k in kept])
+				Y_new=np.array([self.Y_data[k] for k in kept])
+				# ----------- do predictions on the subset ----------
+				probs = self.model.predict(X_new)[:,1]
+				# --------- probability cutoff for simple vs complex -------------
+				predictions = np.where(probs > prob, 1, 0)
+				'''
+				#------------ Confusion Matrix -------------
+				[simple marked as simple 	simple marked as complex]
+				[complex marked as simple 	complex marked as complex]
+				'''
+				cm = confusion_matrix(Y_new, predictions)
+				print(cm)
+				matrix_vals.append(cm)
+			except:
+				print ('Nothing in that cutoff, continuing...')
+		
+		fstring = self._format_param_name(param_name)
+		fig = plt.figure(1)
+		try:
+			plt.scatter(cut_vals,[float(matrix_vals[i][0,0])/(matrix_vals[i][0,0]+matrix_vals[i][0,1])*100. for i in range(len(matrix_vals))],label='True Simple',c='g')
+		except:
+			print ('No simple sources in subsample...')
+		try:
+			plt.scatter(cut_vals,[float(matrix_vals[i][1,1])/(matrix_vals[i][1,0]+matrix_vals[i][1,1])*100. for i in range(len(matrix_vals))],label='True Complex',c='b')
+		except:
+			print ('No complex sources in subsample...')	
+		plt.xlabel(fstring)
+		plt.ylabel('Percent Correct')
+		plt.title(r'Percent Correct over '+fstring)
+		plt.legend(loc=(0.3,0.8),fontsize=5)
 		if save:
-			plt.savefig(imfile)
-			plt.close()
+			plt.savefig(param_name+'_plot.png',bbinches='tight')
 		else:
 			plt.show()
-			plt.close()
+		plt.close()
 
+
+	def _make_2d_cut(self, param_arr1, arr_name1, param_arr2, arr_name2,num_cut=10,prob=0.5,save=False):
+		"""
+		Function for cutting along two parameter values to test the model's performance over
+		a parameter space. For aid in finding parameter space that model works with certainty within.
+		Makes a plot showing the True Positive (TP) and True Negative (TN) rates as a function of the
+		supplied parameters. Functions similarly to _make_cut() above.
+
+		To call:
+			_make_2d_cut(param_arr1, arr_name1, param_arr2, arr_name2, num_cut, prob, save)
+
+		Parameters:
+			param_arr1
+			arr_name1
+			param_arr2
+			arr_name2
+			OPTIONAL:
+				num_cut -- number of cuts to make along the parameter
+				prob -- probability cutoff to classify as complex or simple
+				save -- True if want to save a .pdf
+		"""
+	# ----------- sigma and other params are formatted differently, this handles either case ------
+		try:
+			cut_vals1 = np.linspace(0.,np.max(param_arr1)[0]*.9,num_cut)
+		except:
+			cut_vals1 = np.linspace(0.,np.max(param_arr1)*.9,num_cut)
+		try:
+			cut_vals2 = np.linspace(0.,np.max(param_arr2)[0]*.9,num_cut)
+		except:
+			cut_vals2 = np.linspace(0.,np.max(param_arr2)*.9,num_cut)
+		matrix_vals_c = np.zeros((len(cut_vals1),len(cut_vals2)))
+		matrix_vals_s = np.zeros((len(cut_vals1),len(cut_vals2)))
+		# --------- make a series of cuts and save results for plotting ----------
+		for i in range(len(cut_vals1)):
+			for j in range(len(cut_vals2)):
+				#do the cut
+				c1 = cut_vals1[i]; c2 = cut_vals2[j]
+				float_check = type(0.1); tuple_check = type((0,1))
+				postcut = [];kept=[]
+				for k in range(len(param_arr1)):
+					val1 = param_arr1[k]
+					val2 = param_arr2[k]
+					# ---------- once again handle tuples or floats depending on parameter format ----------
+					if type(val1) == tuple_check:
+						if abs(val1[0]-val1[1]) >= c1 and abs(val2[0]-val2[1]) >= c2:
+							kept.append(k)
+					else:
+						if val1 >= c1 and val2 >= c2:
+							kept.append(k)
+				try:
+					# -------- the subset of data --------------
+					X_new=np.array([self.X_data[k] for k in kept])
+					Y_new=np.array([self.Y_data[k] for k in kept])
+					# ----------- do predictions on the subset ----------
+					probs = self.model.predict(X_new)[:,1]
+					# --------- probability cutoff for simple vs complex -------------
+					predictions = np.where(probs > prob, 1, 0)
+					'''
+					#------------ Confusion Matrix -------------
+					[simple marked as simple 	simple marked as complex]
+					[complex marked as simple 	complex marked as complex]
+					'''
+					cm = confusion_matrix(Y_new, predictions)
+					print(cm)
+					matrix_vals_c[i,j] = float(cm[1,1])/(cm[1,0] +cm[1,1])*100.
+					matrix_vals_s[i,j] = float(cm[0,0])/(cm[0,0] +cm[0,1])*100
+				except:
+					print ('Nothing in that cutoff, continuing...')
+		
+		fstring1 = self._format_param_name(arr_name1)
+		fstring2 = self._format_param_name(arr_name2)
+		xv,yv = np.meshgrid(cut_vals1,cut_vals2)
+		zv_complex =  matrix_vals_c
+		zv_simple = matrix_vals_s
+		#------- show data as an image with z-axis being the TP/TN rates ----
+		fig,ax = plt.subplots(1,2,sharey=True,figsize=(12,7))
+		cax = ax[0].imshow(zv_complex,vmin=50., vmax=100.,cmap='seismic')#,origin='lower')
+		sax = ax[1].imshow(zv_simple,vmin=50., vmax=100.,cmap='seismic')#,origin='lower')
+		# ---- set the axis labels ------
+		ax[0].set_xlabel(fstring1)
+		ax[0].set_ylabel(fstring2)
+		ax[1].set_xlabel(fstring1)
+		ax[1].set_ylabel(fstring2)
+		# ---------- set the tick labels ---------
+		ax[0].set_xticks([n for n in range(len(cut_vals1))])
+		ax[0].set_yticks(range(len(cut_vals2)))
+		ax[1].set_xticks([n for n in range(len(cut_vals2))])
+		xlabels = ['%.2f'%(c) for c in cut_vals1]
+		ylabels = ['%.2f'%(c) for c in cut_vals2]
+		ax[0].set_xticklabels(xlabels)
+		ax[0].set_yticklabels(ylabels)
+		ax[1].set_xticklabels(xlabels)
+		ax[1].set_yticklabels(ylabels)
+		#-------- adjust plot sizing and add colorbar ----------
+		fig.subplots_adjust(right=0.8)
+		cbar_ax = fig.add_axes([0.85, 0.15, 0.05, 0.7])
+		fig.colorbar(cax, cax=cbar_ax)
+
+		ax[0].set_title('Complex Sources')
+		ax[1].set_title('Simple Sources')
+		plt.suptitle(r'Percent Correct over '+fstring1+' and '+fstring2)
+		if save:
+			plt.savefig(arr_name1+'_'+arr_name2+'_plot.png',bbinches='tight')
+		else:
+			plt.show()
+		plt.close()
 
 
 
 if __name__ == '__main__':
 	testing = plots()
 	#testing._loadLog('train.log')
-	testing._plotLoss('train.log')
-	testing._plotAcc('train.log')
+	#testing._plotLoss('train.log',save=False)
+	#testing._plotAcc('train.log',save=False)
+	testing._loadModel('../regularized/model_V1.h5')
+	testing._loadData('/Users/jwisbell/Desktop/aml/faraday/faraday_complexity/data/train/')
+	#testing._make_cut(testing.chi_data, 'chi')
+	testing._make_2d_cut(testing.chi_data[:1000], 'chi',testing.flux_data[:1000], 'flux', num_cut=25)
+
